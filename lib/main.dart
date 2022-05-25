a6d3e8804d5745f212b974cb23f1cb1938be2598
Nancy <chizeanancy@gmail.com>
2022-05-25 23:56:32 +0100

    Add network image to center of scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text("I got you"),
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+        title: Text("Queen Building"),
+        titleTextStyle: TextStyle(color: Colors.black),
+        backgroundColor: Colors.white),
+    body: Container(
+      color: Colors.brown,
+      child: Center(
+        child: Image(
+          image: NetworkImage('https://www.hollywoodreporter.com/wp-content/uploads/2022/05/161008_0134-H-2022.jpg?w=681&h=383&crop=1'),
+        ),
       ),
     ),
-  );
+  )));
 }
