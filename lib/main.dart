4601a41e22cd21caaec05e136574583892ef2f3f
Nancy <chizeanancy@gmail.com>
2023-01-28 16:18:14 +0100

    Update app icon

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,13 @@ void main() {
   runApp(MaterialApp(
       home: Scaffold(
     appBar: AppBar(
-        title: Text("Queen Building"),
+        title: Text("Queen Nancy"),
         titleTextStyle: TextStyle(color: Colors.black),
         backgroundColor: Colors.white),
     body: Container(
       color: Colors.brown,
       child: Center(
-        child: Image(
-          image: NetworkImage('https://www.hollywoodreporter.com/wp-content/uploads/2022/05/161008_0134-H-2022.jpg?w=681&h=383&crop=1'),
-        ),
+        child: Image(image: AssetImage('images/diamond.png')),
       ),
     ),
   )));
